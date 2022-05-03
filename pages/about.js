025e5243fb4cd91fc6696be71979b0860159edfa
Simone Lopes <sisilopes20@gmail.com>
2022-05-02 22:42:37 -0300

    _

--- pages/about.js
+++ pages/about.js
@@ -30,7 +30,7 @@ export async function getStaticProps() {
 
 
     return {
-        props: JSON.parse(JSON.stringify(data.aboutcontents[0])),
+        props: data?.aboutcontents[0],
         revalidate: 10
     };
 }
