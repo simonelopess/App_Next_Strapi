327ed692d84cf1786b7e05639a3e5f13d81c75ee
Simone Lopes <sisilopes20@gmail.com>
2022-05-02 23:16:54 -0300

    _

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -15,24 +15,26 @@ export async function getStaticPaths() {
             "content-type": "application/json"
         },
         body: JSON.stringify({
-            query: `{
-                blogposts {
+            query: `
+            {
+                blogposts{
                     slug
                 }
-            }`
+            }
+            `
         })
     }
 
     const res = await fetch(`${URL}/graphql`, fetchParams);
-    const posts = await res.json();
-    const paths = posts.data.blogposts?.map((post) => {
+    const posts = await res.json()
+    const paths = posts.data.blogposts.map((post) => {
         return { params: { slug: post.slug } }
     })
     //cria rotas dinamicas
     return {
         paths: paths,
         fallback: true
-    };
+    }
 }
 
 export async function getStaticProps({ params }) {
@@ -42,26 +44,29 @@ export async function getStaticProps({ params }) {
             "content-type": "application/json"
         },
         body: JSON.stringify({
-            query: `{
+            query: `
+            {
                 blogposts(where: {slug: "${params.slug}"}){
-                    title
-                    description
-                    blogbody
-                    splash{
-                        url
-                    }
+                  title
+                  description
+                  blogbody
+                  splash{
+                    url
+                  }
                 }
-            }`
+              }
+            `
         })
     }
 
     const res = await fetch(`${URL}/graphql`, fetchParams);
     const { data } = await res.json();
 
+
     return {
         props: data.blogposts[0],
         revalidate: 10
-    }
+    };
 }
 
 export default function Content({ title, blogbody, splash }) {
